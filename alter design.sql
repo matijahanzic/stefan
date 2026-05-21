8e11d452360112fe0b385ff78f990aca0c16b852
rborosak <robert.borosak@rathmann.hr>
2026-05-21 21:42:25 +0200

    Add files via upload

---
+++ alter design.sql
@@ -0,0 +1,22 @@
+alter table design
+add column 8k decimal(7,2) DEFAULT NULL
+
+
+alter table design
+add column 16k decimal(7,2) DEFAULT NULL
+
+alter table design
+add column 32k decimal(7,2) DEFAULT NULL
+
+alter table design
+add column 64k decimal(7,2) DEFAULT NULL
+
+alter table design
+add column 128k decimal(7,2) DEFAULT NULL
+
+alter table design
+add column code varchar(50) DEFAULT 'FOPAC'
+
+
+
+
