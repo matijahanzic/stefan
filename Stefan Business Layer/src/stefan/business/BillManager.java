ac953f4ee0ef3e1bbce69a96a8e4b47faf612c93
rborosak <robert.borosak@rathmann.hr>
2026-06-04 20:35:08 +0200

    Bill business partners fetch fixed

--- Stefan Business Layer/src/stefan/business/BillManager.java
+++ Stefan Business Layer/src/stefan/business/BillManager.java
@@ -28,19 +28,18 @@ public class BillManager {
 
     public boolean SaveBill(stefan.business.objects.Bill bill) {
         try {
-            
-            if (bill.getIdBill()!=null)
-            {
+
+            if (bill.getIdBill() != null) {
                 deleteBill(bill.getIdBill());
             }
-           
+
             stefan.data.Bills newBill = new Bills();
-            newBill.setBillNumber(bill.getBillNumber());           
+            newBill.setBillNumber(bill.getBillNumber());
             newBill.setDate(bill.getDate());
             newBill.setBusinessPartnerId(getBp(bill.getBusinessPartnerId()));
 
             List<stefan.data.Billitems> billItems = new ArrayList<Billitems>();
-          
+
             OrderManager orderManager = new OrderManager();
             for (stefan.business.objects.BillItem item : bill.getBillitemsList()) {
                 stefan.data.Billitems newBillItem = new Billitems();
@@ -48,11 +47,11 @@ public class BillManager {
                 newBillItem.setIdBill(newBill);
                 newBillItem.setPackageNumber(item.getPackageNumber());
                 newBillItem.setParts(item.getParts());
-                newBillItem.setIdOrderItem(orderManager.getOrderItemById(item.getOrderItemId()));               
+                newBillItem.setIdOrderItem(orderManager.getOrderItemById(item.getOrderItemId()));
                 newBillItem.setNiklanje(item.getNiklanje());
                 newBillItem.setPricePerUnit(item.getPricePerPart());
                 newBillItem.setItemOrderNumber(item.getItemOrderNumber());
-                
+
                 billItems.add(newBillItem);
             }
             newBill.setBillitemsList(billItems);
@@ -65,65 +64,62 @@ public class BillManager {
             return false;
         }
     }
-    
-    public List<Bill> getSavedBills()
-    {
-        List<Bill> bills=new ArrayList<Bill>();
+
+    public List<Bill> getSavedBills() {
+        List<Bill> bills = new ArrayList<Bill>();
         Query q = entityManager.createNamedQuery("Bills.findAll");
         List<stefan.data.Bills> BillsDB = q.getResultList();
-        for (Bills billDB : BillsDB) 
-        {
-            Bill b=new Bill();
+        for (Bills billDB : BillsDB) {
+            Bill b = new Bill();
             b.setIdBill(billDB.getIdBill());
             b.setDate(billDB.getDate());
             b.setBillNumber(billDB.getBillNumber());
             b.setBusinessPartnerId(billDB.getBusinessPartnerId().getId());
-            
-            List<BillItem> billItems=new ArrayList<BillItem>();
-            for (Billitems bi : billDB.getBillitemsList()) 
-            {
+
+            List<BillItem> billItems = new ArrayList<BillItem>();
+            for (Billitems bi : billDB.getBillitemsList()) {
                 BillItem item = OrderManager.populateBillItem(bi.getIdOrderItem());
                 item.setParts(bi.getParts());
-                item.setPackageNumber(bi.getPackageNumber());      
+                item.setPackageNumber(bi.getPackageNumber());
                 item.setNiklanje(bi.getNiklanje());
                 item.setItemOrderNumber(bi.getItemOrderNumber());
                 item.setPricePerPart(bi.getPricePerUnit());
-                
-               
-                billItems.add(item);                              
+
+
+                billItems.add(item);
             }
-            b.setBillitemsList(billItems);           
-            
+            b.setBillitemsList(billItems);
+
             bills.add(b);
         }
-        
-        return bills;        
+
+        return bills;
     }
-    
-    public stefan.data.Bills getBillById(Integer Id)
-    {
-       Query q = entityManager.createNamedQuery("Bills.findByIdBill");
-       q.setParameter("idBill", Id);          
-       List<stefan.data.Bills> items = q.getResultList();
-       return items.get(0);   
+
+    public stefan.data.Bills getBillById(Integer Id) {
+        Query q = entityManager.createNamedQuery("Bills.findByIdBill");
+        q.setParameter("idBill", Id);
+        List<stefan.data.Bills> items = q.getResultList();
+        return items.get(0);
     }
-    
-    private stefan.data.Businesspartner getBp(Integer id){
+
+    private stefan.data.Businesspartner getBp(Integer id) {
         Query q = entityManager.createNamedQuery("Businesspartner.findById");
         q.setParameter("id", id);
         List<stefan.data.Businesspartner> bps = q.getResultList();
         return bps.get(0);
     }
-    
-     public List<BusinessPartner> getBusinessPartners()
-    {
-       List<BusinessPartner> bpList = new ArrayList<BusinessPartner>();
-       
+
+    public List<BusinessPartner> getBusinessPartners() {
+        List<BusinessPartner> bpList = new ArrayList<BusinessPartner>();
+
         Query q = entityManager.createNamedQuery("Businesspartner.findAll");
         List<stefan.data.Businesspartner> bps = q.getResultList();
-        for (stefan.data.Businesspartner item : bps) 
-        {
-            stefan.business.objects.BusinessPartner bp =new BusinessPartner();
+        for (stefan.data.Businesspartner item : bps) {
+            if (item.getIsExternalSource() == true) {
+                continue;
+            }
+            stefan.business.objects.BusinessPartner bp = new BusinessPartner();
             bp.setDisplayName(item.getDisplayName());
             bp.setId(item.getId());
             bp.setName(item.getName());
@@ -133,26 +129,20 @@ public class BillManager {
             bp.setPrintRow3(item.getPrintRow3());
             bp.setCity(item.getCity());
             bp.setRequireShippingDate(item.getRequireShippingDate());
-            
+
             bpList.add(bp);
         }
-        
-        return bpList;        
+
+        return bpList;
     }
-    
-     
-    public void deleteBill(Integer id)
-    {
-        try
-        {             
-             Bills bill=getBillById(id);
-             entityManager.getTransaction().begin();
-             entityManager.remove(bill);
-             entityManager.getTransaction().commit();
-        } 
-        catch(Exception e)
-        {
+
+    public void deleteBill(Integer id) {
+        try {
+            Bills bill = getBillById(id);
+            entityManager.getTransaction().begin();
+            entityManager.remove(bill);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
         }
-    }    
-    
+    }
 }
